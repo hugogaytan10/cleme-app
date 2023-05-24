9747c5c81e1ca7e557686b271b394c94a26e07f6
hugogaytan10 <estardus9@gmail.com>
2023-05-24 13:27:06 -0600

    fc

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,26 +1,164 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useEffect, useRef, useState } from 'react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonList, IonPage, IonPopover, IonRefresher, IonRefresherContent, IonRow, IonSegment, IonSegmentButton, IonSelect, IonTitle, IonToast, IonToolbar, RefresherEventDetail } from '@ionic/react';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { isPlatform } from '@ionic/react';
+import { Network } from '@capacitor/network';
 import './Tab1.css';
-
+interface CatImage {
+  height: number;
+  id: string;
+  url: string;
+  width: number;
+}
 const Tab1: React.FC = () => {
+  const [cats, setCats] = useState<CatImage[]>();
+  const [selectedSegment, setSelectedSegment] = useState('default');
+  const [isOpen, setIsOpen] = useState(false);
+  const [net, setNet] = useState(true);
+  const popover = useRef<HTMLIonPopoverElement>(null);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const openPopover = (e: any) => {
+    popover.current!.event = e;
+    setPopoverOpen(true);
+  };
+  const GetData = async () => {
+    try {
+      const url = 'https://api.thecatapi.com/v1/images/search?limit=10';
+      const peticion = await fetch(url);
+      const response = await peticion.json();
+      setCats(response);
+      setSelectedSegment('default');
+    } catch (error: any) { console.log(error) }
+  }
+  const GetDataDogs = async () => {
+    try {
+      const url = 'https://api.thedogapi.com/v1/images/search?limit=10';
+      const peticion = await fetch(url);
+      const response = await peticion.json();
+      setCats(response);
+      setSelectedSegment('segment');
+    } catch (error: any) { console.log(error) }
+  }
+  const saveImageToDevice = async (url: string, fileName: string): Promise<void> => {
+    try {
+      let base64Data: string;
+      if (isPlatform('hybrid')) {
+        const file = await Filesystem.readFile({
+          path: url!,
+        });
+        base64Data = file.data;
+      } else {
+        base64Data = await base64FromPath(url!);
+      }
+      const savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data,
+      });
+
+      setIsOpen(true);
+
+    } catch (error) {
+      console.error('Error al guardar la imagen:', error);
+    }
+  }
+  async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+  function handleRefresh(event: CustomEvent<RefresherEventDetail>) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      GetData();
+      event.detail.complete();
+    }, 2000);
+  }
+  Network.addListener('networkStatusChange', status => {
+    status.connected ? setNet(true) : setNet(false);
+    console.log(status)
+  });
+  useEffect(() => {
+    GetData();
+  }, [])
+  useEffect(() => {
+    console.log(net);
+    GetData();
+  }, [net])
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Gatitos y Perritos</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+      {
+        net
+          ?
+          <IonContent className="ion-padding">
+            <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+              <IonRefresherContent></IonRefresherContent>
+            </IonRefresher>
+            <IonSegment value={selectedSegment}>
+              <IonSegmentButton value="default" onClick={() => { GetData() }}>
+                <IonLabel>Gatos</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value="segment" onClick={() => { GetDataDogs() }}>
+                <IonLabel>Perros</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
+            <IonGrid>
+              <IonRow className="ion-justify-content-center ion-align-items-center">
+                {
+                  cats != undefined &&
+                  cats.map((cat, idx) => {
+                    return (
+                      <IonCol key={idx} size='12' sizeXl='3' sizeMd='4'>
+                        <IonCard style={{ minWidth: '95%' }}>
+                          <div style={{ display: 'flex', justifyContent: 'center' }} onClick={()=>{setPopoverOpen(true)}}>
+                            <img alt="Silhouette of mountains" src={cat.url} style={{ width: '100%', height: '250px', objectFit: 'contain' }} />
+                          </div>
+                          <IonCardContent >
+                            <IonButton style={{ margin: 'auto', display: 'block' }} onClick={() => { saveImageToDevice(cat.url, cat.id) }}>Guardar</IonButton>
+                            <IonToast
+                              isOpen={isOpen}
+                              message="Ya se guardó, bato"
+                              onDidDismiss={() => setIsOpen(false)}
+                              duration={5000}
+                            ></IonToast>
+                            <IonPopover ref={popover} isOpen={popoverOpen} onDidDismiss={() => setPopoverOpen(false)}>
+                              <IonContent class="ion-padding">hola!</IonContent>
+                            </IonPopover>
+                          </IonCardContent>
+                        </IonCard>
+                      </IonCol>
+                    )
+                  })
+                }
+              </IonRow>
+            </IonGrid>
+          </IonContent>
+          :
+          <IonContent>
+            <IonItem>No hay net :c</IonItem>
+          </IonContent>
+      }
+
     </IonPage>
   );
 };
+
 
 export default Tab1;
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
+import { Clipboard } from '@capacitor/clipboard';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const [location, setLocation] = useState("Click para ubicacion");
+  const [input, setInput] = useState("");
+  const printCurrentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+    return coordinates;
+  };
+  const openSite = async () => {
+    await Browser.open({ url: 'https://www.facebook.com/' });
+  };
+  async function writeToClipboard(text:string){
+    console.log(text)
+  await Clipboard.write({
+    string: text
+  });
+}
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Plugins a lo buey</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div style={{ display: 'flex', justifyContent: "space-between" }}>
+          <IonInput placeholder={location} ></IonInput>
+          <IonButton onClick={async () => { setLocation(await printCurrentPosition().then(pos => { return pos.coords.longitude + " " + pos.coords.latitude; })) }}>Ubicación</IonButton>
+        </div>
+        <IonButton onClick={() => { openSite() }}>Open Site</IonButton>
+        <div style={{display: 'flex',  justifyContent: "space-between"}}>
+        <IonInput placeholder='escribe algo...' onIonChange={(e)=>{if(e.target.value != undefined) setInput(e.target.value.toString())}}></IonInput>
+        <IonButton onClick={()=>{writeToClipboard(input)}}>COPIAR</IonButton>
+       </div>
       </IonContent>
     </IonPage>
   );
